ad99d1db74f23ef18aee269a0e9206359e8e0f72
Jakub Drągowski <kuba.dragowski@gmail.com>
2018-12-30 01:13:53 +0100

    Beginning- First commit check. Add basic files.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("driving", function() {
+    console.log("I'm driving");
+    return gulp.src("js/*.js")
+            .pipe(jshint())
+            .pipe(jshint.reporter("default"));
+});
+
+gulp.task("sass", function() {
+    console.log("Działam i mam się dobrze ;)");
+    return gulp.src("scss/*.scss")
+           .pipe(sourcemaps.init())
+           .pipe(sass({
+               errLogToConsole: true,
+               outputStyle: "expanded",
+               sourceComments: "map"
+    }))
+           .pipe(sourcemaps.write())
+           .pipe(gulp.dest("css"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch("scss/*scss", gulp.series("sass"));
+});
