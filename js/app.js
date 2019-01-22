48bf6d0858636212f13950167fc5d5629ba320eb
Jakub Drągowski <kuba.dragowski@gmail.com>
2019-01-23 00:14:46 +0100

    css changed, add arrows learn sass

--- js/app.js
+++ js/app.js
@@ -7,5 +7,19 @@ console.log(4 + 4);
 //require('bootstrap');
 //require('bootstrap');
 
+$(document).ready(function() {
+    console.log("DOM loaded");
+    
+//    Global variables
+    var $body = $("html, body");
+    var $scrollUp = $('.btn-scroll-up');
+    
+    
+    //scrolling to the top of page after refresh website
+//    $body.stop().animate({scrollTop:0}, "500");
 
- 
+    //Scrolling to the top of page after click button
+    $scrollUp.click(function() {
+        $body.stop().animate({scrollTop:0}, "500");
+    });
+});
