54d8fc9ebb2743413fe4d30f7dfe9c2c324e8ca9
Jakub Drągowski <kuba.dragowski@gmail.com>
2019-02-10 15:03:15 +0100

    added responsive, fonts, last changes, php, rems, so much to end

--- js/app.js
+++ js/app.js
@@ -53,6 +53,43 @@ $(document).ready(function() {
         var position = $(jumpTo).position().top;
         $body.animate({scrollTop: position}, 1000);
     });
+    
+    
+//    SLIDER
+    var next = $("#next"),
+    prev = $("#prev"),
+    currentIndex = 0,
+    imgNumber = $(".slider").find("div").length,
+    img = $(".slider").find("div"),
+    width = $(".slider").width();
+    
+    //console.log(imgNumber);
+    //
+    //NEXT    
+        next.on("click", function(event){
+          img.eq(currentIndex).css("display", "none") 
+          img.eq(currentIndex+1).css("display", "inline-block");
+                    currentIndex++;
+            //console.log(currentIndex);
+            if (currentIndex > (imgNumber-1)) {
+                currentIndex = 0;
+                            //console.log("po przeładowaniu " + currentIndex);
+            }
+                            img.eq(currentIndex).css("display", "inline-block");
+
+      })
+    //PREV   
+        prev.on("click", function(event){
+          img.eq(currentIndex).css("display", "none") 
+          currentIndex--;
+                    //console.log(currentIndex);
+                    if (currentIndex < 0) {
+                            currentIndex = imgNumber-1;
+                            //console.log(currentIndex);
+                    }
+          img.eq(currentIndex).css("display", "inline-block");
+      })
+    
 });
 
 
